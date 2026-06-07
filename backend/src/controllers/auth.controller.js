58d83f4064731cbdebc6a2672a2834f75efad1ba
Sampurn17 <sampurn.samadder@gmail.com>
2026-06-08 01:02:59 +0530

    fix: set secure and sameSite none on auth cookies for cross-domain support

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -35,7 +35,11 @@ async function registerUserController(req,res){
         {expiresIn:"1d"}
     )
 
-    res.cookie("token",token)
+    res.cookie("token",token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    })
     res.status(201).json({
         message:"User registered successfully",
         user:{
@@ -70,7 +74,11 @@ async function loginUserController(req,res){
         {expiresIn:"1d"}
     )
 
-    res.cookie("token",token)
+    res.cookie("token",token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    })
     res.status(201).json({
         message:"User logged In successfully",
         user:{
@@ -94,7 +102,11 @@ async function logoutUserController(req,res){
         await tokenBlacklistModel.create({token})
     }
 
-    res.clearCookie("token");
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+    });
     res.status(200).json({
         message: "User logged out successfully"
     })
